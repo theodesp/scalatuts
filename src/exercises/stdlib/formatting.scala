db955d96e6130e964b53f741f34a9211681b1ccf
Theo <theo.despoudis@teckro.com>
2019-08-22 20:41:29 +0100

    Formatting

--- src/exercises/stdlib/formatting.scala
+++ src/exercises/stdlib/formatting.scala
@@ -1,3 +1,21 @@
 object formatting extends App {
+  val s = "Hello World"
 
+  "Application %s".format(s)
+
+  val a = 'a'
+  val b = 'B'
+
+  "%c".format(a)
+  "%c".format(b)
+
+  val c = 'a' //unicode for a
+  val d = '\u0061' //octal for a
+  val e = '\"'
+  val f = '\\'
+
+  "%c".format(c)
+  "%c".format(d)
+  "%c".format(e)
+  "%c".format(f)
 }
---
+++ src/exercises/stdlib/patternMatching.scala
@@ -0,0 +1,3 @@
+object patternMatching extends App {
+
+}
