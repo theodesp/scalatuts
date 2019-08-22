fb8a3f2d6f8c9dea71873be0d265385d623fa9cb
Theo <theo.despoudis@teckro.com>
2019-08-22 17:38:50 +0100

    Maps

--- src/exercises/stdlib/maps.scala
+++ src/exercises/stdlib/maps.scala
@@ -1,3 +1,24 @@
 object maps extends App {
+  val myMap =
+    Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+  myMap.size // 4
 
+  val aNewMap = myMap + ("IL" → "Illinois")
+
+  val mapValues = myMap.values
+
+  mapValues.head
+  mapValues.last
+
+  myMap("MI")
+
+  val myMap2 = Map("Ann Arbor" → "MI", 49931 → "MI")
+
+  myMap.getOrElse("TX", "missing data")
+
+  val aNewMap2 = myMap - "MI"
+
+  aNewMap2.contains("MI") // false
+
+  val aNewMap3 = myMap - ("MI", "WI")
 }
