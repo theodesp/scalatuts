63f5206ba4ee2d11a2bb411972632da09f41d9bb
Theo <theo.despoudis@teckro.com>
2019-08-22 21:03:37 +0100

    Case Classes

--- src/exercises/stdlib/caseClasses.scala
+++ src/exercises/stdlib/caseClasses.scala
@@ -1,3 +1,47 @@
 object caseClasses extends App {
+  abstract class Term
+  case class Var(name: String) extends Term
+  case class Fun(arg: String, body: Term) extends Term
+  case class App(f: Term, v: Term) extends Term
 
+  val x1 = Var("x")
+  val x2 = Var("x")
+  val y1 = Var("y")
+  println("" + x1 + " == " + x2 + " => " + (x1 == x2))
+  println("" + x1 + " == " + y1 + " => " + (x1 == y1))
+
+  def printTerm(term: Term) {
+    term match {
+      case Var(n) =>
+        print(n)
+      case Fun(x, b) =>
+        print("^" + x + ".")
+        printTerm(b)
+      case App(f, v) =>
+        print("(")
+        printTerm(f)
+        print(" ")
+        printTerm(v)
+        print(")")
+    }
+  }
+  def isIdentityFun(term: Term): Boolean = term match {
+    case Fun(x, Var(y)) if x == y => true
+    case _ => false
+  }
+  val id = Fun("x", Var("x"))
+  val t = Fun("x", Fun("y", App(Var("x"), Var("y"))))
+  printTerm(t)
+  println
+  println(isIdentityFun(id))
+  println(isIdentityFun(t))
+
+  case class Dog(name: String, breed: String)
+  val d1 = Dog("Scooby", "Doberman")
+  d1.toString
+
+  case class Person(first: String, last: String, age: Int = 0, ssn: String = "")
+  val p1 = Person("Fred", "Jones", 23, "111-22-3333")
+
+  p1.isInstanceOf[Serializable]
 }
